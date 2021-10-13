c9a493f5a62531bbc637ba26fe68d8d740ee546e
aksamitsah <aksamitsah@gmail.com>
2021-10-13 14:17:21 +0545

    Initial commit

---
+++ app/src/main/java/me/aksamitsah/sqldatabase/Adapter/CourseRVAdapter.java
@@ -0,0 +1,95 @@
+package me.aksamitsah.sqldatabase.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import me.aksamitsah.sqldatabase.R;
+import me.aksamitsah.sqldatabase.SqlLiteOperation.CourseModal;
+import me.aksamitsah.sqldatabase.UpdateCourseActivity;
+
+public class CourseRVAdapter extends RecyclerView.Adapter<CourseRVAdapter.ViewHolder> {
+
+    // variable for our array list and context
+    private final ArrayList<CourseModal> courseModalArrayList;
+    private final Context context;
+
+    // constructor
+    public CourseRVAdapter(ArrayList<CourseModal> courseModalArrayList, Context context) {
+        this.courseModalArrayList = courseModalArrayList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // on below line we are inflating our layout
+        // file for our recycler view items.
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.course_rv_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // on below line we are setting data
+        // to our views of recycler view item.
+        CourseModal modal = courseModalArrayList.get(position);
+        holder.courseNameTV.setText(modal.getCourseName());
+        holder.courseDescTV.setText(modal.getCourseDescription());
+        holder.courseDurationTV.setText(modal.getCourseDuration());
+        holder.courseTracksTV.setText(modal.getCourseTracks());
+
+        // below line is to add on click listener for our recycler view item.
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // on below line we are calling an intent.
+                Intent i = new Intent(context, UpdateCourseActivity.class);
+
+                // below we are passing all our values.
+                i.putExtra("name", modal.getCourseName());
+                i.putExtra("description", modal.getCourseDescription());
+                i.putExtra("duration", modal.getCourseDuration());
+                i.putExtra("tracks", modal.getCourseTracks());
+
+                // starting our activity.
+                context.startActivity(i);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        // returning the size of our array list
+        return courseModalArrayList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        // creating variables for our text views.
+        private final TextView courseNameTV;
+        private final TextView courseDescTV;
+        private final TextView courseDurationTV;
+        private final TextView courseTracksTV;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            // initializing our text views
+            courseNameTV = itemView.findViewById(R.id.idTVCourseName);
+            courseDescTV = itemView.findViewById(R.id.idTVCourseDescription);
+            courseDurationTV = itemView.findViewById(R.id.idTVCourseDuration);
+            courseTracksTV = itemView.findViewById(R.id.idTVCourseTracks);
+        }
+    }
+}
+
---
+++ app/src/main/java/me/aksamitsah/sqldatabase/UpdateCourseActivity.java
@@ -0,0 +1,77 @@
+package me.aksamitsah.sqldatabase;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import me.aksamitsah.sqldatabase.SqlLiteOperation.DBHandler;
+
+public class UpdateCourseActivity extends AppCompatActivity {
+
+    String courseName, courseDesc, courseDuration, courseTracks;
+    // variables for our edit text, button, strings and dbhandler class.
+    private EditText courseNameEdt, courseTracksEdt, courseDurationEdt, courseDescriptionEdt;
+    private Button updateCourseBtn, deleteCourseBtn;
+    private DBHandler dbHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_course);
+
+        // initializing all our variables.
+        courseNameEdt = findViewById(R.id.idEdtCourseName);
+        courseTracksEdt = findViewById(R.id.idEdtCourseTracks);
+        courseDurationEdt = findViewById(R.id.idEdtCourseDuration);
+        courseDescriptionEdt = findViewById(R.id.idEdtCourseDescription);
+        updateCourseBtn = findViewById(R.id.idBtnUpdateCourse);
+        deleteCourseBtn = findViewById(R.id.idBtnDelete);
+
+        // on below line we are initialing our dbhandler class.
+        dbHandler = new DBHandler(UpdateCourseActivity.this);
+
+        // on below lines we are getting data which
+        // we passed in our adapter class.
+        courseName = getIntent().getStringExtra("name");
+        courseDesc = getIntent().getStringExtra("description");
+        courseDuration = getIntent().getStringExtra("duration");
+        courseTracks = getIntent().getStringExtra("tracks");
+
+        // setting data to edit text
+        // of our update activity.
+        courseNameEdt.setText(courseName);
+        courseDescriptionEdt.setText(courseDesc);
+        courseTracksEdt.setText(courseTracks);
+        courseDurationEdt.setText(courseDuration);
+
+        // adding on click listener to our update course button.
+        updateCourseBtn.setOnClickListener(v -> {
+
+            // inside this method we are calling an update course
+            // method and passing all our edit text values.
+            dbHandler.updateCourse(courseName, courseNameEdt.getText().toString(), courseDescriptionEdt.getText().toString(), courseTracksEdt.getText().toString(), courseDurationEdt.getText().toString());
+
+            // displaying a toast message that our course has been updated.
+            Toast.makeText(UpdateCourseActivity.this, "Course Updated..", Toast.LENGTH_SHORT).show();
+
+            // launching our main activity.
+            Intent i = new Intent(UpdateCourseActivity.this, ViewCourses.class);
+            startActivity(i);
+        });
+
+        // adding on click listener for delete button to delete our course.
+        deleteCourseBtn.setOnClickListener((View.OnClickListener) v -> {
+            // calling a method to delete our course.
+            dbHandler.deleteCourse(courseName);
+            Toast.makeText(UpdateCourseActivity.this, "Deleted the course", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(UpdateCourseActivity.this, AddData.class);
+            startActivity(i);
+        });
+
+    }
+}
